ca0397441e5b989766005b8f346cc916cd482ff9
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2025-04-10 10:05:29 -0400

    fix(images): removed reusable image component (#17)

--- images.ts
+++
@@ -1,6 +0,0 @@
-import { glob } from 'glob'
-import { writeFile } from 'fs/promises'
-
-const images = await glob("**/*.png", { ignore: 'node_modules/**' })
-
-await writeFile('images.mdx', images.map(image => `![](/${image})`).join('\n'))
