e02881d5925f24e667c452bab0434ac5f0ed0cf3
Francois Levasseur <francois.levasseur@botpress.com>
2025-04-22 16:15:29 -0400

    update

---
+++ .github/lol.js
@@ -0,0 +1,19 @@
+var ok = {
+  text: 'Docs Update',
+  channel: 'C08K7SB0VT8',
+  blocks: [
+    {
+      type: 'markdown',
+      text: ':blue_book: [franklevasseur](https://github.com/franklevasseur) updated the docs!',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'markdown',
+      text: 'im just testing the ci',
+    },
+  ],
+}
+
+console.log(JSON.stringify(ok))
