ef7dbcb94a627fb933d383c417d682f37766f8fe
Adrian Kahali <adrian.kahali@hotmail.com>
2025-06-20 10:47:42 -0400

    feat: add bot to docs (#134)

---
+++ bot.js
@@ -0,0 +1,50 @@
+// Inject the Botpress Webchat script
+const webchatScript = document.createElement('script')
+webchatScript.src = 'https://cdn.botpress.cloud/webchat/v3.0/inject.js'
+webchatScript.async = true
+webchatScript.defer = true
+
+function loadWebchat() {
+  window.botpress.init({
+      botId: 'e3098b37-2e06-4351-859c-d79e06f39ca8',
+      configuration: {
+        version: 'v1',
+        composerPlaceholder: 'Ask a question...',
+        botName: 'Assistant',
+        botAvatar: 'https://files.bpcontent.cloud/2025/06/16/20/20250616204038-BRUW6C2R.svg',
+        botDescription: 'Ask AI a question about the documentation.',
+        website: {},
+        email: {
+          title: '',
+          link: '',
+        },
+        phone: {},
+        termsOfService: {
+          title: 'Terms of service',
+          link: 'google.com',
+        },
+        privacyPolicy: {},
+        color: '#0588F0',
+        variant: 'soft',
+        headerVariant: 'glass',
+        themeMode: 'light',
+        fontFamily: 'inter',
+        radius: 3,
+        feedbackEnabled: true,
+        footer: '[⚡️ by Botpress](https://botpress.com/?from=webchat)',
+        additionalStylesheetUrl: 'https://files.bpcontent.cloud/2025/06/13/14/20250613145950-XC43YPI7.css',
+        allowFileUpload: true,
+      },
+      clientId: '44246de9-1d1b-462c-8ef3-1ce39e65d89a',
+  })
+};
+
+webchatScript.onload = () => {
+  if (window.botpress && typeof window.botpress.init === 'function') {
+    loadWebchat()
+  } else {
+    console.error('Botpress init not available')
+  }
+}
+
+document.body.appendChild(webchatScript)
